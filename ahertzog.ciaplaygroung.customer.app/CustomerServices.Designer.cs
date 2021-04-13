317c7f3528f58bd8711ed4fd3e7abdfef29abd16
inatan <inatan_hertzog@hotmail.com>
2021-04-13 20:35:30 -0300

    Update window layout

--- ahertzog.ciaplaygroung.customer.app/CustomerServices.Designer.cs
+++ ahertzog.ciaplaygroung.customer.app/CustomerServices.Designer.cs
@@ -29,21 +29,85 @@ namespace ahertzog.ciaplaygroung.customer.app
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonGerar = new System.Windows.Forms.Button();
+            this.buttonProcurar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonFechar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(81, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(433, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonGerar
+            // 
+            this.buttonGerar.Location = new System.Drawing.Point(8, 75);
+            this.buttonGerar.Name = "buttonGerar";
+            this.buttonGerar.Size = new System.Drawing.Size(149, 23);
+            this.buttonGerar.TabIndex = 1;
+            this.buttonGerar.Text = "Gerar Excel de Clientes";
+            this.buttonGerar.UseVisualStyleBackColor = true;
+            this.buttonGerar.Click += new System.EventHandler(this.ButtonGerar_Click);
+            // 
+            // buttonProcurar
+            // 
+            this.buttonProcurar.Location = new System.Drawing.Point(520, 35);
+            this.buttonProcurar.Name = "buttonProcurar";
+            this.buttonProcurar.Size = new System.Drawing.Size(75, 23);
+            this.buttonProcurar.TabIndex = 1;
+            this.buttonProcurar.Text = "Procurar";
+            this.buttonProcurar.UseVisualStyleBackColor = true;
+            this.buttonProcurar.Click += new System.EventHandler(this.ButtonProcurar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pasta Origem";
+            // 
+            // buttonFechar
+            // 
+            this.buttonFechar.Location = new System.Drawing.Point(520, 75);
+            this.buttonFechar.Name = "buttonFechar";
+            this.buttonFechar.Size = new System.Drawing.Size(75, 23);
+            this.buttonFechar.TabIndex = 1;
+            this.buttonFechar.Text = "Fechar";
+            this.buttonFechar.UseVisualStyleBackColor = true;
+            this.buttonFechar.Click += new System.EventHandler(this.ButtonFechar_Click);
             // 
             // CustomerServices
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 124);
+            this.ClientSize = new System.Drawing.Size(615, 110);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonProcurar);
+            this.Controls.Add(this.buttonFechar);
+            this.Controls.Add(this.buttonGerar);
+            this.Controls.Add(this.textBox1);
             this.Name = "CustomerServices";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Conversor de Clientes";
+            this.Load += new System.EventHandler(this.FormCustomer_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonGerar;
+        private System.Windows.Forms.Button buttonProcurar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonFechar;
     }
 }
 
--- ahertzog.ciaplaygroung.customer.app/CustomerServices.cs
+++ ahertzog.ciaplaygroung.customer.app/CustomerServices.cs
@@ -17,7 +17,22 @@ namespace ahertzog.ciaplaygroung.customer.app
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void FormCustomer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonGerar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonProcurar_Click(object sender, EventArgs e)
         {
 
         }
